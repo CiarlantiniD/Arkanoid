415a7a80bb928119b6f825172e0451a3b5e0f383
CiarlantiniD <dario.ciarlantini@outlook.com>
2018-12-24 20:03:00 -0300

    Se crea Script de Canvas

---
+++ Arkanoid/Assets/Scripts/Canvas_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvas_script : MonoBehaviour
+{
+    public static Canvas_script instance = null;
+
+    Text scoreText;
+    void Awake()
+    {
+  if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy (gameObject);
+
+        scoreText = transform.GetChild(0).GetComponent<Text>();
+    }
+
+    public void Score(int scoreValue){
+        scoreText.text = scoreValue.ToString();
+    }
+
+}
