7afe12a9746fa7e3190b33be9d86f19912f77a0e
CiarlantiniD <dario.ciarlantini@outlook.com>
2019-01-07 23:58:49 -0300

    Prueba de PowerUp

---
+++ Arkanoid/Assets/Scripts/Power_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power_Script : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+    }
+
+   
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.03f, transform.position.z);
+    }
+
+/*     void OnCollisionEnter(Collision collision){
+
+         Debug.Log(collision.gameObject.name );
+
+        if(collision.gameObject.name == "Base"){
+
+            Destroy(this);
+        }  
+     } */
+
+      void OnTriggerEnter(Collider col){
+
+          Debug.Log(col.gameObject.name);
+
+          Debug.Log("Atravezo el Trigger");
+          gameObject.SetActive(false);
+      }
+
+
+}
