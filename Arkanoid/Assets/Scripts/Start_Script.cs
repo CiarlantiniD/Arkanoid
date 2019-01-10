57feb0737815ff5bfe8f84c4787a6e71a1df9c6a
CiarlantiniD <dario.ciarlantini@outlook.com>
2019-01-10 05:47:36 -0300

    Creación del Menu Principal

---
+++ Arkanoid/Assets/Scripts/Start_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Start_Script : MonoBehaviour
+{
+
+    private Text highScoreText;
+
+    void Awake(){
+        highScoreText = transform.GetChild(2).GetComponent<Text>();
+        Score(PlayerPrefs.GetInt("highScore"));
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return)){
+            SceneManager.LoadScene("Game",LoadSceneMode.Single);
+        }
+    }
+
+    private void Score(int scoreValue){
+
+        string ZeroString = "";
+        int ZerosInScore = 8 - scoreValue.ToString().Length;       
+
+        for (int i = 0; i < ZerosInScore; i++){
+            ZeroString = "0" + ZeroString; 
+        }
+
+        highScoreText.text = "High Score: " + ZeroString + scoreValue.ToString() + "00";
+    }
+}
